cb2d61fd7576d0c7de56cdff5d59deca7b5fe30c
Putnam3145 <bpwhite@cableone.net>
2014-09-21 15:36:50 -0700

    Updated scouter.

--- hack/scripts/dragonball/scouter.lua
+++ hack/scripts/dragonball/scouter.lua
@@ -1,14 +1,14 @@
 -- Gives power level of selected unit. Type "accurate" for a more linear, but more boring number.
 
-local selectedUnit = dfhack.gui.getSelectedUnit(silent)
+local utils=require('utils')
 
-local args = {...}
+validArgs = utils.invert({
+ 'accurate',
+ 'all',
+ 'citizens'
+})
 
-local accurate = false
-
-for _,arg in ipairs(args) do
-    if arg == "accurate" then accurate = true end
-end
+local args = utils.processArgs({...}, validArgs)
 
 local function unitIsGod(unit)
     local unitraws = df.creature_raw.find(unit.race)
@@ -28,20 +28,16 @@ end
 
 --power levels should account for disabilities and such
 local function isWinded(unit)
-    if unit.counters.winded > 0 then return true end
-    return false
+    return unit.counters.winded > 0
 end
 local function isStunned(unit)
-    if unit.counters.stunned > 0 then return true end
-    return false
+    return unit.counters.stunned > 0
 end
 local function isUnconscious(unit)
-    if unit.counters.unconscious > 0 then return true end
-    return false
+    return unit.counters.unconscious > 0
 end
 local function isParalyzed(unit)
-    if unit.counters2.paralysis > 0 then return true end
-    return false
+    return unit.counters2.paralysis > 0
 end
 local function getExhaustion(unit)
     local exhaustion = 1
@@ -52,44 +48,59 @@ local function getExhaustion(unit)
     return 1
 end
 
-if not selectedUnit then
-    qerror('Need to select a unit.')
-end
-
 --blood_max appears to be the creature's body size divided by 10; the power level calculation relies on body size divided by 1000, so divided by 100 it is. blood_count refers to current blood amount, and it, when full, is equal to blood_max.
-if selectedUnit.curse.attr_change then
-	strength = ((selectedUnit.body.physical_attrs.STRENGTH.value+selectedUnit.curse.attr_change.phys_att_add.STRENGTH)/3550)*(selectedUnit.curse.attr_change.phys_att_perc.STRENGTH/100)
-	endurance = ((selectedUnit.body.physical_attrs.ENDURANCE.value+selectedUnit.curse.attr_change.phys_att_add.ENDURANCE)/1000)*(selectedUnit.curse.attr_change.phys_att_perc.ENDURANCE/100)
-	toughness = ((selectedUnit.body.physical_attrs.TOUGHNESS.value+selectedUnit.curse.attr_change.phys_att_add.TOUGHNESS)/2250)*(selectedUnit.curse.attr_change.phys_att_perc.TOUGHNESS/100)
-	spatialsense = ((selectedUnit.status.current_soul.mental_attrs.SPATIAL_SENSE.value+selectedUnit.curse.attr_change.ment_att_add.SPATIAL_SENSE)/1500)*(selectedUnit.curse.attr_change.ment_att_perc.SPATIAL_SENSE/100)
-	kinestheticsense = ((selectedUnit.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value+selectedUnit.curse.attr_change.ment_att_add.KINESTHETIC_SENSE)/1000)*(selectedUnit.curse.attr_change.ment_att_perc.KINESTHETIC_SENSE/100)
-	willpower = ((selectedUnit.status.current_soul.mental_attrs.WILLPOWER.value+selectedUnit.curse.attr_change.ment_att_add.WILLPOWER)/1000)*(selectedUnit.curse.attr_change.ment_att_perc.WILLPOWER/100)
+
+local function getPowerLevel(saiyan,accurate)
+	if accurate then
+		local strength = saiyan.body.physical_attrs.STRENGTH.value/3550
+		local endurance = saiyan.body.physical_attrs.ENDURANCE.value/1000
+		local toughness = saiyan.body.physical_attrs.TOUGHNESS.value/2250
+		local spatialsense = saiyan.status.current_soul.mental_attrs.SPATIAL_SENSE.value/1500
+		local kinestheticsense = saiyan.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value/1000
+		local willpower = saiyan.status.current_soul.mental_attrs.WILLPOWER.value/1000
+		return (strength+endurance+toughness+spatialsense+kinestheticsense+willpower)
+	else
+		local strength,endurance,toughness,spatialsense,kinestheticsense,willpower
+		if saiyan.curse.attr_change then
+			strength = ((saiyan.body.physical_attrs.STRENGTH.value+saiyan.curse.attr_change.phys_att_add.STRENGTH)/3550)*(saiyan.curse.attr_change.phys_att_perc.STRENGTH/100)
+			endurance = ((saiyan.body.physical_attrs.ENDURANCE.value+saiyan.curse.attr_change.phys_att_add.ENDURANCE)/1000)*(saiyan.curse.attr_change.phys_att_perc.ENDURANCE/100)
+			toughness = ((saiyan.body.physical_attrs.TOUGHNESS.value+saiyan.curse.attr_change.phys_att_add.TOUGHNESS)/2250)*(saiyan.curse.attr_change.phys_att_perc.TOUGHNESS/100)
+			spatialsense = ((saiyan.status.current_soul.mental_attrs.SPATIAL_SENSE.value+saiyan.curse.attr_change.ment_att_add.SPATIAL_SENSE)/1500)*(saiyan.curse.attr_change.ment_att_perc.SPATIAL_SENSE/100)
+			kinestheticsense = ((saiyan.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value+saiyan.curse.attr_change.ment_att_add.KINESTHETIC_SENSE)/1000)*(saiyan.curse.attr_change.ment_att_perc.KINESTHETIC_SENSE/100)
+			willpower = ((saiyan.status.current_soul.mental_attrs.WILLPOWER.value+saiyan.curse.attr_change.ment_att_add.WILLPOWER)/1000)*(saiyan.curse.attr_change.ment_att_perc.WILLPOWER/100)
+		else
+			strength = saiyan.body.physical_attrs.STRENGTH.value/3550
+			endurance = saiyan.body.physical_attrs.ENDURANCE.value/1000
+			toughness = saiyan.body.physical_attrs.TOUGHNESS.value/2250
+			spatialsense = saiyan.status.current_soul.mental_attrs.SPATIAL_SENSE.value/1500
+			kinestheticsense = saiyan.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value/1000
+			willpower = saiyan.status.current_soul.mental_attrs.WILLPOWER.value/1000
+		end
+		local exhaustion = getExhaustion(saiyan)
+		local bodysize = (saiyan.body.blood_count/100)^2
+		powerlevel = bodysize*((strength*endurance*toughness*spatialsense*kinestheticsense*willpower)^(1/6))*exhaustion
+		if isWinded(saiyan) then powerlevel=powerlevel/1.2 end
+		if isStunned(saiyan) then powerlevel=powerlevel/1.5 end
+		if isParalyzed(saiyan) then powerlevel=powerlevel/5 end
+		if isUnconscious(saiyan) then powerlevel=powerlevel/10 end
+		if powerlevel == 1/0 or unitIsGod(saiyan) then
+			dfhack.gui.showPopupAnnouncement("The scouter broke at this incredible power. Either the power belongs to a god... or it's immeasurable.",1)
+			qerror("Scouter broke! Oh well, there are more.",11)
+		end
+		return math.floor(powerlevel)
+	end
+end
+
+if args.all then
+	for k,v in ipairs(df.global.world.units.active) do
+		print(dfhack.TranslateName(dfhack.units.getVisibleName(v))..' has a power level of '..getPowerLevel(v,args.accurate))
+	end
+elseif args.citizens then
+	for k,v in ipairs(df.global.world.units.active) do
+		if dfhack.units.isCitizen(v) then
+			print(dfhack.TranslateName(dfhack.units.getVisibleName(v))..' has a power level of '..getPowerLevel(v,args.accurate))
+		end
+	end
 else
-	strength = selectedUnit.body.physical_attrs.STRENGTH.value/3550
-	endurance = selectedUnit.body.physical_attrs.ENDURANCE.value/1000
-	toughness = selectedUnit.body.physical_attrs.TOUGHNESS.value/2250
-	spatialsense = selectedUnit.status.current_soul.mental_attrs.SPATIAL_SENSE.value/1500
-	kinestheticsense = selectedUnit.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value/1000
-	willpower = selectedUnit.status.current_soul.mental_attrs.WILLPOWER.value/1000
+	dfhack.gui.showPopupAnnouncement("The scouter says " .. getPowerLevel(dfhack.gui.getSelectedUnit(silent),args.accurate) .. "!",11)
 end
-local exhaustion = getExhaustion(selectedUnit)
-
-
-if accurate == true
-then
-    powerlevel = strength+endurance+toughness+spatialsense+kinestheticsense+willpower
-else
-    local bodysize = (selectedUnit.body.blood_count/100)^2
-    powerlevel = bodysize*((strength*endurance*toughness*spatialsense*kinestheticsense*willpower)^(1/6))*exhaustion
-end
-if isWinded(selectedUnit) then powerlevel=powerlevel/1.2 end
-if isStunned(selectedUnit) then powerlevel=powerlevel/1.5 end
-if isParalyzed(selectedUnit) then powerlevel=powerlevel/5 end
-if isUnconscious(selectedUnit) then powerlevel=powerlevel/10 end
-
-if powerlevel == 1/0 or unitIsGod(selectedUnit) then
-    dfhack.gui.showPopupAnnouncement("The scouter broke at this incredible power. Either the power belongs to a god... or it's immeasurable.",1)
-    qerror("Scouter broke! Oh well, there are more.",11)
-end
-
-dfhack.gui.showAnnouncement("The scouter says " .. math.floor(powerlevel) .. "!",11)
