cb65a875bd0976ea54f1910478318b9dbbad815e
Putnam3145 <bpwhite@cableone.net>
2015-04-15 00:45:06 -0700

    Made super saiyan trigger rely also on agility.
    Also reworked difficulty to get to that level a little.

--- raw/scripts/dragonball/super_saiyan_trigger.lua
+++ raw/scripts/dragonball/super_saiyan_trigger.lua
@@ -1,16 +1,18 @@
 local utils=require('utils')
 local function getPowerLevel(saiyan)
-    local strength = saiyan.body.physical_attrs.STRENGTH.value/3550
+    local strength = saiyan.body.physical_attrs.STRENGTH.value/1000
+    local agility = saiyan.body.physical_attrs.AGILITY.value/1000
     local endurance = saiyan.body.physical_attrs.ENDURANCE.value/1000
-    local toughness = saiyan.body.physical_attrs.TOUGHNESS.value/2250
-    local spatialsense = saiyan.status.current_soul.mental_attrs.SPATIAL_SENSE.value/1500
+    local toughness = saiyan.body.physical_attrs.TOUGHNESS.value/1000
+    local spatialsense = saiyan.status.current_soul.mental_attrs.SPATIAL_SENSE.value/1000
     local kinestheticsense = saiyan.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value/1000
     local willpower = saiyan.status.current_soul.mental_attrs.WILLPOWER.value/1000
-    return (strength+endurance+toughness+spatialsense+kinestheticsense+willpower)
+    return (strength+agility+endurance+toughness+spatialsense+kinestheticsense+willpower)/13.85
 end
+
 local function unitHasSyndrome(u,s_name)
     for k,syn in ipairs(u.syndromes.active) do
-        if df.global.world.raws.syndromes.all[syn.type].syn_name==s_name then return true end
+        if df.syndrome.find(syn.type).syn_name==s_name then return true end
     end
     return false
 end
@@ -25,17 +27,17 @@ local unit = df.unit.find(args.unit)
 
 local powerLevel=getPowerLevel(unit)
 
-if powerLevel>100 then
+if powerLevel>10 then
     dfhack.run_script('modtools/add-syndrome','-syndrome','can super saiyan 3','-resetPolicy','DoNothing','-target',args.unit,'-skipImmunities')
 end
-if powerLevel>50 then
+if powerLevel>5 then
     if not unitHasSyndrome(unit,'can super saiyan 2') then
         dfhack.run_script('modtools/add-syndrome','-syndrome','can super saiyan 2','-resetPolicy','DoNothing','-target',args.unit,'-skipImmunities')
         dfhack.gui.makeAnnouncement(df.announcement_type.MARTIAL_TRANCE,{PAUSE=true,RECENTER=true,D_DISPLAY=true,A_DISPLAY=true,DO_MEGA=true},unit.pos,dfhack.TranslateName(dfhack.units.getVisibleName(unit))..' has transformed into a Super Saiyan 2 in a bout of extreme emotion!',11)
     end
     dfhack.run_script('modtools/add-syndrome','-syndrome','Super Saiyan 2','-resetPolicy','DoNothing','-target',args.unit,'-skipImmunities')
 end
-if powerLevel>20 then
+if powerLevel>2 then
     if not unitHasSyndrome(unit,'can super saiyan') then
         dfhack.run_script('modtools/add-syndrome','-syndrome','Super Saiyan','-resetPolicy','DoNothing','-target',args.unit,'-skipImmunities')
         dfhack.gui.makeAnnouncement(df.announcement_type.MARTIAL_TRANCE,{PAUSE=true,RECENTER=true,D_DISPLAY=true,A_DISPLAY=true,DO_MEGA=true},unit.pos,dfhack.TranslateName(dfhack.units.getVisibleName(unit))..' has transformed into a Super Saiyan in a bout of extreme emotion!',11)
