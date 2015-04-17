b1a8494f740d52af30be9f117ff0fc678fc8f5a8
Putnam3145 <bpwhite@cableone.net>
2015-04-17 01:06:20 -0700

    Added action event.
    Oh, this is going to be so fun.

---
+++ raw/scripts/dragonball/unit_action_check.lua
@@ -0,0 +1,24 @@
+onUnitAction=dfhack.event.new()
+
+local actions_already_checked=actions_already_checked or {}
+
+local function action_already_checked(unit_id,action_id)
+    local unit_action_checked=actions_already_checked[unit_id]
+    if unit_action_checked then
+        return unit_action_checked[action_id]
+    end
+end
+
+local function checkForActions()
+    for k,v in ipairs(df.global.world.units.active) do
+        for _,action in ipairs(v.actions) do
+            if not action_already_checked(v.id,action.id) then
+                onUnitAction(v,action)
+                actions_already_checked[v.id]=actions_already_checked[v.id] or {}
+                actions_already_checked[v.id][action.id]=true
+            end
+        end
+    end
+end
+
+require('repeat-util').scheduleEvery('onAction',1,'ticks',checkForActions) --surprisingly fast
