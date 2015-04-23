e1fc37dc0fe870f624f6c9d9a25df66aec952efe
Putnam3145 <bpwhite@cableone.net>
2015-04-22 22:49:57 -0700

    Various tweaks related to DFHack leftovers.

--- raw/scripts/dragonball/cell_absorb.lua
+++ raw/scripts/dragonball/cell_absorb.lua
@@ -5,12 +5,11 @@ local unit = df.unit.find(args[1])
 
 if df.creature_raw.find(unit.race).creature_id~="CELL" then dfhack.error("Creature absorbing isn't Cell! Report this error with the stack trace!",nil,true) end
 
-unit.body.blood_max=unit.body.blood_max+300
-
-for k,v in ipairs(unit.body.size_info) do
-	v=v*math.log(math.ceil((v/1000*math.exp(1))))
-end
-
-for k,v in ipairs(unit.body.physical_attrs) do
-	v.value=v.value+500
-end
+unit.body.physical_attrs.STRENGTH.value=round(unit.body.physical_attrs.STRENGTH.value*+100)
+unit.body.physical_attrs.AGILITY.value=round(unit.body.physical_attrs.AGILITY.value+100)
+unit.body.physical_attrs.ENDURANCE.value=round(unit.body.physical_attrs.ENDURANCE.value+100)
+unit.body.physical_attrs.TOUGHNESS.value=round(unit.body.physical_attrs.TOUGHNESS.value+100)
+unit.status.current_soul.mental_attrs.SPATIAL_SENSE.value=round(unit.status.current_soul.mental_attrs.SPATIAL_SENSE.value+100)
+unit.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value=round(unit.status.current_soul.mental_attrs.KINESTHETIC_SENSE.value+100)
+unit.status.current_soul.mental_attrs.WILLPOWER.value=round(unit.status.current_soul.mental_attrs.WILLPOWER.value+100)
+unit.status.current_soul.mental_attrs.FOCUS.value=round(unit.status.current_soul.mental_attrs.FOCUS.value+100)
