f2c10e6c31ec0e37455a2859cec6bb24335b197f
Putnam3145 <bpwhite@cableone.net>
2015-04-24 20:16:53 -0700

    Allowed super saiyans to happen without emotion.
    Again. Ugh.

--- raw/init.d/sparking.lua
+++ raw/init.d/sparking.lua
@@ -489,6 +489,7 @@ function checkEveryUnitRegularlyForEvents()
         checkIfUnitStillGravelyInjuredForZenkai(v)
         checkOverflows(v)
         chargeKi(v.id)
+        dfhack.script_environment('dragonball/super_saiyan_trigger').runSuperSaiyanChecks(v.id)
     end
 end
 
@@ -569,7 +570,7 @@ eventful.enableEvent(eventful.eventType.UNIT_DEATH,5)
 function onStateChange(op)
     if op==SC_MAP_LOADED then
         dfhack.script_environment('dragonball/unit_action_check').enableEvent()
-        require('repeat-util').scheduleEvery('DBZ Event Check',100,'ticks',checkEveryUnitRegularlyForEvents)
+        require('repeat-util').scheduleEvery('DBZ Event Check',150,'ticks',checkEveryUnitRegularlyForEvents)
     end
 end
 
