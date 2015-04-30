b852ab625c65df70b0689d221c9e1f89882be097
Putnam3145 <bpwhite@cableone.net>
2015-04-29 17:01:09 -0700

    Added a ki adjusting GUI.

---
+++ raw/scripts/dragonball/adv_ki_adjust.lua
@@ -0,0 +1,52 @@
+local ki = dfhack.script_environment('dragonball/ki')
+
+local gui=require('gui')
+
+local widgets=require('gui.widgets')
+
+local kiViewScreen=defclass(kiViewScreen,gui.FramedScreen)
+
+function kiViewScreen:onInput(keys)
+    if keys.LEAVESCREEN then
+        self:dismiss()
+    else
+        self:inputToSubviews(keys)
+    end
+end
+
+function kiViewScreen:init(args)
+    self.kiInvestment=tostring(ki.get_unit_ki_persist_entry(df.global.world.units.active[0].id).ints[3])
+    local kiText='Ki investment (higher is less, 1 is all at once):'
+    self.kiInvestText='Ki investment currently set to '
+    local adventureID=df.global.world.units.active[0].id
+    self.investmentLabel=widgets.Label{
+        frame={t=1},
+        text=self.kiInvestText..self.kiInvestment
+    }
+    self:addviews{
+        widgets.Label{
+            frame={t=0},
+            text=kiText
+        },
+        widgets.EditField{
+            frame={t=0,l=#kiText+1},
+            text=self.kiInvestment,
+            on_submit=function(text)
+                if tonumber(text) and tonumber(text)>1 then
+                    ki.set_ki_investment(adventureID,tonumber(text))
+                    self.kiInvestment=tostring(ki.get_unit_ki_persist_entry(adventureID).ints[3])
+                    self.investmentLabel:setText(self.kiInvestText..self.kiInvestment)
+                end
+            end
+        },
+        self.investmentLabel,
+        widgets.Label{
+            frame={t=2},
+            text='Current ki amount: ' .. ki.get_ki(adventureID)..'/'..ki.get_max_ki(adventureID)..' ('..100*ki.get_ki(adventureID)/ki.get_max_ki(adventureID)..'%)'
+        }
+    }
+end
+
+local kiScreen=kiViewScreen{frame_title='Ki settings',frame_width=80,frame_height=3}
+
+kiScreen:show()
