6f315b882dc4176d9aa47533814d12a47000d202
Putnam3145 <bpwhite@cableone.net>
2017-12-10 13:19:38 -0800

    actually made dragon balls have the cooldown whoops

--- raw/scripts/dragonball/wish.lua
+++ raw/scripts/dragonball/wish.lua
@@ -257,6 +257,7 @@ function makeAWish(unit,adventure)
     local last_wish_found=dfhack.persistent.save({key='DRAGONBALL_LAST_WISH_TIME'})
     if last_wish_found.ints[1]==df.global.cur_year or (last_wish_found.ints[1]==df.global.cur_year-1 and last_wish_found.ints[2]<df.global.cur_year_tick) then
         dfhack.gui.makeAnnouncement(df.announcement_type.ERA_CHANGE,{DO_MEGA=true,RECENTER=true,PAUSE=true},unit.pos,'The dragon balls still need to recharge!',COLOR_LIGHTGREEN)
+        return false
     end
     local script=require('gui.script')
     script.start(function()
