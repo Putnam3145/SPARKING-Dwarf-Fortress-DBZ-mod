0a580cc787e1c63e2206ad16f7e6b6d64852af41
Putnam <putnam3145@gmail.com>
2018-08-31 11:54:00 -0700

    fixed up wishes a little

--- raw/scripts/dragonball/wish.lua
+++ raw/scripts/dragonball/wish.lua
@@ -14,6 +14,7 @@ local function immortalityWish(adventure)
             end
         end
         table.sort(citizen_list,function(a,b) return getPowerLevel(df.unit.find(a[3]))>getPowerLevel(df.unit.find(b[3])) end)
+        local script=require('gui.script')
         local citizen_okay,_,citizen_table=script.showListPrompt('Wish','Which citizen do you wish to make immortal?',COLOR_LIGHTGREEN,citizen_list)
         if citizen_okay then
             dfhack.persistent.save({key='DRAGONBALL_IMMORTAL/'..citizen_table[3]})
@@ -39,6 +40,7 @@ local function ressurectionWish(pos)
         end
     end
     table.sort(dead_people_list,function(a,b) return (a and b) and getPowerLevel(df.unit.find(a[3]))>getPowerLevel(df.unit.find(b[3])) or false end)
+    local script=require('gui.script')
     local dead_okay,_,dead_table=script.showListPrompt('Wish','Who do you wish to revive?',COLOR_LIGHTGREEN,dead_people_list)
     if dead_okay then
         dfhack.run_script('full-heal','-r','-unit',dead_table[3])
@@ -51,8 +53,9 @@ local function ressurectionWish(pos)
     return false
 end
 
-function hackWish(unit)
-    dfhack.run_script('gui/create-item','unit',unit.id)
+function hackWish(unit,callback)
+    dfhack.run_script('gui/create-item','-unit',unit.id,'-restrictive','-multi')
+    return true
 end
 
 function makeAWish(unit,adventure)
@@ -64,19 +67,16 @@ function makeAWish(unit,adventure)
     end
     local script=require('gui.script')
     script.start(function()
-    for i=1,3 do
-        local okay=true
-        repeat
-            okay,selection=script.showListPrompt('Wish','Choose your wish.',COLOR_GREEN,{'Items','Immortality','Ressurection'})
-            if selection==1 then
-                okay=hackWish(unit)
-            elseif selection==2 then
-                okay=immortalityWish(adventure)
-            else
-                okay=ressurectionWish(unit.pos)
-            end
-        until okay
-    end
+    repeat
+        local okay,selection=script.showListPrompt('Wish','Choose your wish.',COLOR_GREEN,{'Item','Immortality','Ressurection'})
+        if selection==1 then
+            okay=hackWish(unit)
+        elseif selection==2 then
+            okay=immortalityWish(adventure)
+        elseif selection==3 then
+            okay=ressurectionWish(unit.pos)
+        end
+    until okay
     local wishes=dfhack.persistent.save({key='DRAGONBALL_WISH_COUNT'})
     wishes.ints[1]=wishes.ints[1]+1
     wishes:save()
